e394af47ca01d8f5858e0667f0632356bbd2a642
Gemma Fong <115029730+smolTypo@users.noreply.github.com>
2023-02-14 15:14:42 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,62 @@
+function App(){
+  const [todos, setTodos] = React.useState([
+    {
+      text: 'persimmons',
+      isCompleted: false,
+    },
+    {
+      text: 'black garlic',
+      isCompleted: false,
+    },
+    {
+      text: 'cultured butter',
+      isCompleted: false,
+    },        
+    {
+      text: 'poached pears',
+      isCompleted: false,
+    },   
+    {
+      text: 'panettone french toast',
+      isCompleted: false,
+    }, 
+    {
+      text: 'roasted squash',
+      isCompleted: false,
+    }, 
+    {
+      text: 'tahini desserts',
+      isCompleted: false,
+    }, 
+    {
+      text: 'earl grey',
+      isCompleted: false,
+    },  
+  ]);
+
+  const addTodo = text => {
+    const newTodos = [...todos, {text, isCompleted:false}];
+    setTodos(newTodos);
+  }
+  const removeTodo = index => {
+    let temp = [...todos];    
+    temp.splice(index, 1);
+    setTodos(temp);
+  }
+
+  return(
+    <div className="app">
+      <div className="todo-list" >
+        {todos.map((todo, i) => (
+          <Todo key={i} index={i} todo={todo} remove={removeTodo}/>
+        ))}
+        <TodoForm addTodo={addTodo} />
+      </div>
+    </div>
+  );
+}
+
+ReactDOM.render(
+  <App/>,
+  document.getElementById('root')
+);
